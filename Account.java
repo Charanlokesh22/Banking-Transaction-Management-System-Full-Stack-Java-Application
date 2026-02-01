f18961a3500e3c6e975082220907536280f1a0c4
Charanlokesh22 <charanlokesh2918@gmail.com>
2026-02-01 12:10:41 +0530

    Add Account class for bank model

---
+++ Account.java
@@ -0,0 +1,25 @@
+package com.bank.model;
+
+public class Account {
+
+    private int accountId;
+    private String holderName;
+    private double balance;
+
+    public Account() {}
+
+    public Account(int accountId, String holderName, double balance) {
+        this.accountId = accountId;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    public int getAccountId() { return accountId; }
+    public void setAccountId(int accountId) { this.accountId = accountId; }
+
+    public String getHolderName() { return holderName; }
+    public void setHolderName(String holderName) { this.holderName = holderName; }
+
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) { this.balance = balance; }
+}
